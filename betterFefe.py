da22e6dbb6a242cbe7b3d0a73d4e747804c475dc
dodonator <dodo@dodo@chaosdorf.de>
2013-06-06 20:13:06 +0200

    better

--- betterFefe.py
+++ betterFefe.py
@@ -8,11 +8,10 @@ class fefe(object):
 		self.webSeitenInhalt = self.webSeite.read()
 		self.webSeitenInhalt = self.webSeitenInhalt.replace("""<h2><a href="/" style="text-decoration:none;color:black">Fefes Blog</a></h2>
 <b>Wer schöne Verschwörungslinks für mich hat: ab an felix-bloginput (at) fefe.de!</b>
-
-<p align=right>Fragen?  <a href="/faq.html">Antworten!</a>  Siehe auch: <a href="http://alternativlos.org/">Alternativlos</a><p>""",'')
-		self.webSeitenInhalt = webSeitenInhalt.split('/<ul>')
+<p align=right>Fragen?  <a href="/faq.html">Antworten!</a>  Siehe auch: <a href="http://alternativlos.org/">Alternativlos</a><p>""",'\n')
+		self.webSeitenInhalt = self.webSeitenInhalt.split('/<ul>')
 	def markiere(self,toMark):
-		for inhalt in webSeitenInhalt:
+		for inhalt in self.webSeitenInhalt:
 			inhalt = inhalt.replace(toMark,'<b>' + toMark + '</b>')
 	def archivPutzen(self):
 		self.archiv = open('archiv.html','w')
@@ -23,15 +22,12 @@ class fefe(object):
 		archiv = open('archiv.html','r')
 		self.archiv = archiv.read()
 		archiv.close()
-		# Dateischreiber für den Momentanen Tag!
-		self.tmpTag = open('dodosBlog.html','w')
-		self.tmpTag.write(self.webSeitenInhalt)
-		self.tmpTag.close()
 		# Dateischreiber für das Archiv
-		if self.webSeitenInhalt not in self.archiv:
-			self.sappend('archiv.html',self.webSeitenInhalt, 3)
-		else:
-			pass
+		for inhalt in self.webSeitenInhalt:
+			if inhalt not in self.archiv:
+				self.sappend('archiv.html',inhalt, 3)
+			else:
+				pass
 	def main(self):
 		self.lese()
 		eingabe = raw_input('')
@@ -59,10 +55,4 @@ class fefe(object):
 		
 	
 f = fefe()
-for i in range(100):
-	f.main()
-	time.sleep(60)
-
-		
-
-
+f.main()
