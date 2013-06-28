93c527c0218bf1942a7edf2cb2ebba28cdf286a6
dodonator <dodo@dodo@chaosdorf.de>
2013-06-28 21:22:40 +0200

    test

--- gitFoo.py
+++ gitFoo.py
@@ -1,7 +1,7 @@
 import os
 # os.system('')
 commitMessage = raw_input('CommitMessage: \n')
-os.system('git add --all')
+# os.system('git add --all')
 os.system('git commit -a -m "' + commitMessage + '"')
 os.system('git push')
 os.system('clear')
