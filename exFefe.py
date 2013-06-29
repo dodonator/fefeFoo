d01545a8f23c5d7868d4acbf1d42e3dcfee17f0c
dodonator <dodo@dodo@chaosdorf.de>
2013-06-29 17:24:01 +0200

    Verschönerung

--- exFefe.py
+++ exFefe.py
@@ -5,6 +5,8 @@ import time
 import datetime
 import os
 
+# 200701 Beispiel
+
 monate = ['01','02','03','04','05','06','07','08','09','10','11','12']
 jahre = []
 startJ = '2006'
@@ -28,32 +30,45 @@ inhaltsverzeichnis = []
 os.system('touch start.html')
 files = ['start.html']
 counter = 0
+# os.system('mkdir Sites')
+currentDir = os.getcwd()
+ordnerInhalt = os.listdir('./Sites')
 for monat in resultMonate:
 	tmpFilename = 'fefe' + monat + '.html'
+	tmpUnterVerzeichnis = './Sites/' + tmpFilename
 	files.append(tmpFilename)
-	tmpCommand = 'touch ' + tmpFilename
-	tmpPath = 'file:///home/dodo/git/fefeFoo/' + tmpFilename
-	tmpHyperlink = '<a href=' + tmpFilename + '>' + monat + '</a>' + '\n'
-	os.system(tmpCommand)
-	site = urllib.urlopen('http://blog.fefe.de/?mon=' + monat)
-	result = site.read()
-	file1 = open(tmpFilename,'a')
-	f1 = file1.write(result)
-	file1.close()
+	if tmpFilename not in ordnerInhalt:
+		tmpCommand = 'touch ' + tmpFilename
+		os.system(tmpCommand)
+		site = urllib.urlopen('http://blog.fefe.de/?mon=' + monat)
+		result = site.read()
+		file1 = open(tmpUnterVerzeichnis,'w')
+		f1 = file1.write(result)
+		file1.close()
+	tmpPath = 'file://' + currentDir + '/Sites/' + tmpFilename
+	tmpHyperlink = '<a href=' + tmpPath + '>' + monat + '</a>' + '\n'
 	inhaltsverzeichnis.append(tmpHyperlink)
+# print inhaltsverzeichnis
 dateiInhalt = ''
-dateiInhalt += "<h3> Inhaltsverzeichnis von Fefe's Blog! </h3><br> "
+dateiInhalt += '<html><head><title>Fefe Archiv</title></head><body>'
+dateiInhalt += "<h3> Inhaltsverzeichnis von Fefe's Blog! </h3><br>"
+dateiInhalt += '<br>'
+dateiInhalt += '<table border="1">'
 dateCounter = 0
 yearCounter = 0
+dateiInhalt += '<tr>'
+for i in range(1,13):
+	dateiInhalt += '<th>' + str(i) + '</th>'
+dateiInhalt += '</tr><tr>'
 for link in inhaltsverzeichnis:
 	dateCounter += 1
 	if dateCounter == 12:
-		dateiInhalt += link + ' ' + str(int(startJ)+yearCounter) + '<br>'
+		dateiInhalt += '<th>' + link + ' ' + '</th><th>' + str(int(startJ)+yearCounter) + '</th></tr>'
 		dateCounter = 0
 		yearCounter += 1
 	else:
-		dateiInhalt += link + ' '
-
+		dateiInhalt += '<th>' + link + '</th>'
+dateiInhalt += '</body></html>'
 file2 = open('start.html','w')
 f2 = file2.write(dateiInhalt)
 file2.close()
