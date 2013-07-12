038fd8fd68aecc1361f6cee639bc85aa0cc835ee
dodonator <dodo@dodo@chaosdorf.de>
2013-07-12 11:41:07 +0200

    Dokumentierung

--- exFefe.py
+++ exFefe.py
@@ -1,13 +1,15 @@
 # coding: utf-8
-
+#1-#
 import urllib
 import time
 import datetime
 import os
+#-1#
 
 # 200701 Beispiel für monat
 # fefe200701.html Beispiel für tmpFilename
 
+#2-#
 monate = ['01','02','03','04','05','06','07','08','09','10','11','12']
 jahre = []
 startJ = '2006'
@@ -25,15 +27,17 @@ for mon in range(1,time.localtime()[1]+1):
 		mon = str(0) + str(mon)
 
 	resultMonate.append(str(str(time.localtime()[0]) + str(mon)))
-
 jahr = ''
 monat = ''
+#-2#
+
 inhaltsverzeichnis = []
 os.system('touch start.html')
 files = ['start.html']
 counter = 0
-# os.system('mkdir Sites')
 currentDir = os.getcwd()
+if 'Sites' not in os.listdir(currentDir):
+	os.system('mkdir Sites')
 ordnerInhalt = os.listdir('./Sites')
 for monat in resultMonate:
 	tmpFilename = 'fefe' + monat + '.html'
@@ -50,12 +54,14 @@ for monat in resultMonate:
 	tmpPath = 'file://' + currentDir + '/Sites/' + tmpFilename
 	tmpHyperlink = '<a href=' + tmpPath + '>' + monat + '</a>' + '\n'
 	inhaltsverzeichnis.append(tmpHyperlink)
+#4-#
 sitesInhalt = os.listdir('./Sites')
 lastMonth = resultMonate[len(resultMonate)-1]
 lastMonthPDF = 'fefe' + lastMonth + '.pdf'
 if lastMonthPDF not in sitesInhalt:
 	os.system('wkhtmltopdf ' + './Sites/fefe' + lastMonth + '.html ' + './Sites/' + lastMonthPDF)
-# print inhaltsverzeichnis
+#-4#
+
 dateiInhalt = ''
 dateiInhalt += '<html><head><title>Fefe Archiv</title></head><body>'
 dateiInhalt += "<h3> Inhaltsverzeichnis von Fefe's Blog! </h3><br>"
@@ -79,7 +85,3 @@ dateiInhalt += '</body></html>'
 file2 = open('start.html','w')
 f2 = file2.write(dateiInhalt)
 file2.close()
-def removeMonthFiles(files):
-	for datei in files:
-		os.system('sudo rm ' + datei)
-#removeMonthFiles(files)
